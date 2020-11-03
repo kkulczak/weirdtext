586090cf72b1b7ffd5200b4e2dde2495646fe02d
kkulczak <kulczakkacper@gmail.com>
2020-11-03 14:50:05 +0100

    docstrings

--- translation_engine.py
+++ translation_engine.py
@@ -8,6 +8,14 @@ SEPARATOR = '\n—weird—\n'
 
 
 def encode(message: str) -> str:
+    '''
+    This function encodes the message in weired-text format. Every word above
+    length 3 will have permuted inner letters. At the end of return message
+    we append a sequence of correct words which can be used to decode the
+    message.
+    :param message: Message to be encoded
+    :return: Encoded message
+    '''
     # Tokenization
     tokenize_re = re.compile(r'(\w+)', re.U)
     tokenized = tokenize_re.finditer(message)
@@ -45,10 +53,24 @@ def encode(message: str) -> str:
 
 
 def unique_rep(w: str) -> str:
+    '''
+    Unique format which allow us to represent any permuteted word as the same
+    string.
+    :param w: word
+    :return: unique representation of word
+    '''
     return f"{w[0]}{''.join(sorted(w[1:-1]))}{w[-1]}"
 
 
-def decode(message:str) -> str:
+def decode(message: str) -> str:
+    '''
+This function decodes strings in weired-text format. message have to contain
+2 magic separators `\n—weird—\n`. After first one should be encoded content,
+and after second one we expect a sequence of correct words, from which we can
+decode the content.
+    :param message: Encoded message in weired-text format.
+    :return: Decoded message
+    '''
     # Validation
     if len(re.findall(SEPARATOR, message)) != 2:
         raise ValueError('Wrong number of seprarators')
